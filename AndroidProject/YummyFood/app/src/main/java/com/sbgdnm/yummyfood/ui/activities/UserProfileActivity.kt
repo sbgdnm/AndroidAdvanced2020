326df106a18e29eab8ba01040d5ac214c11054ba
sbgdnm <sbm.overlord@gmail.com>
2020-12-06 20:12:12 +0500

    Загрузка фото для профиля пользователя.

--- AndroidProject/YummyFood/app/src/main/java/com/sbgdnm/yummyfood/ui/activities/UserProfileActivity.kt
+++ AndroidProject/YummyFood/app/src/main/java/com/sbgdnm/yummyfood/ui/activities/UserProfileActivity.kt
@@ -1,29 +1,40 @@
 package com.sbgdnm.yummyfood.ui.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.sbgdnm.yummyfood.R
 import com.sbgdnm.yummyfood.models.User
 import com.sbgdnm.yummyfood.utils.Constants
 import kotlinx.android.synthetic.main.activity_user_profile.*
+import java.io.IOException
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
 
 
-        //Retrieve the User details from intent extra.
-        // Create a instance of the User model class.
+        //Ивзлекаем данные пользователя
+        //сперва создаем экзампляр класса пользователя
         var userDetails: User = User()
         if(intent.hasExtra(Constants.EXTRA_USER_DETAILS)) {
-            // Get the user details from intent as a ParcelableExtra.
+            // Получием сведения о пользователе из intent в виде ParcelableExtra.
             userDetails = intent.getParcelableExtra(Constants.EXTRA_USER_DETAILS)!!
         }
 
 
-        //After receiving the user details from intent set it to the UI.
-        // Here, the some of the edittext components are disabled because it is added at a time of Registration.
+        //После получения реквизитов пользователей показываем данные на интерфейсе
+        // Здесь некоторые компоненты edittext отключены, поскольку они добавляются во время регистрации.
         et_first_name.isEnabled = false     //их нельзя менять так как они являются статичными , мы их вводим при регистрации
         et_first_name.setText(userDetails.firstName)
 
@@ -32,6 +43,101 @@ class UserProfileActivity : AppCompatActivity() {
 
         et_email.isEnabled = false
         et_email.setText(userDetails.email)
+
+        // Назначьте событие on click фотографии профиля пользователя.
+        iv_user_photo.setOnClickListener(this@UserProfileActivity)
+    }
+    //Override the onClick функция для события добавленее фотографии
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+
+                R.id.iv_user_photo -> {
+
+                    // Здесь мы проверим, разрешено ли разрешение уже или нам нужно его запросить.
+                    // Прежде всего мы проверим разрешение READ_EXTERNAL_STORAGE, и если оно не будет разрешено, мы запросим его
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                        == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        //вызовием функцию выбора изображения (из constants), когда у пользователя уже есть разрешение на чтение хранилища.
+                        Constants.showImageChooser(this@UserProfileActivity)
+                    } else {
+
+                        /*Запрашивает разрешения, которые должны быть предоставлены этому приложению.
+                        Эти разрешения должны быть запрошены в вашем манифесте, они не должны быть предоставлены вашему приложению,
+                         и они должны иметь уровень защиты*/
+
+
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    // Override функция для идентификации результата разрешения среды выполнения после того,
+    // как пользователь разрешает или запрещает разрешение на основе уникального кода.
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //Если разрешение уже есть
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //вызывием функцию выбора изображения (из constants), когда у пользователя уже есть разрешение на чтение хранилища.
+                Constants.showImageChooser(this@UserProfileActivity)
+            } else {
+                //если разрешения не дали
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    // Получаем результат после выбора изображения из памяти телефона, используя уникальный код, который мы передали в момент выбора через intent.
+    /**
+     * @param requestCode integer запрос ,предоставленный startActivityForResult (), позволяет определить, от кого пришел этот результат
+     * @param resultCode integer код результата
+     * @param data Intent,которое может возвращать вызывающему данные результата
+     */
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Constants.PICK_IMAGE_REQUEST_CODE) {
+                if (data != null) {
+                    try {
+                        // Uri выбранного изображения из памяти телефона.
+                        val selectedImageFileUri = data.data!!
+
+                        iv_user_photo.setImageURI(Uri.parse(selectedImageFileUri.toString()))
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        Toast.makeText(
+                            this@UserProfileActivity,
+                            resources.getString(R.string.image_selection_failed),
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            // когда пользователь закрывает или отменяет выбор изображения.
+            Log.e("Запрос Отменен", "Выбор изображения отменяется")
+        }
     }
 
 }
--- AndroidProject/YummyFood/app/src/main/java/com/sbgdnm/yummyfood/utils/Constants.kt
+++ AndroidProject/YummyFood/app/src/main/java/com/sbgdnm/yummyfood/utils/Constants.kt
@@ -1,5 +1,9 @@
 package com.sbgdnm.yummyfood.utils
 
+import android.app.Activity
+import android.content.Intent
+import android.provider.MediaStore
+
 object Constants {
 
     // Firebase Constants
@@ -12,4 +16,24 @@ object Constants {
 
     // Intent extra constants.
     const val EXTRA_USER_DETAILS: String = "extra_user_details"
+
+
+    //Уникальный код для запроса разрешения на чтение хранилища с помощью этого мы будем проверять и идентифицировать в методе onRequestPermissionsResult в base activity
+    const val READ_STORAGE_PERMISSION_CODE = 2
+
+
+    //уникальный код для выбора изображения из хранилища. Используя этот код, мы идентифицируем URI изображения, как только он будет выбран.
+    //Уникальный код выбора изображения из памяти телефона.
+    const val PICK_IMAGE_REQUEST_CODE = 1
+
+    //функция для запроса выбрать изображение с помощью уникального кода.(Функция выбора изображения профиля пользователя из памяти телефона.)
+    fun showImageChooser(activity: Activity) {
+        // для запуска выбора изображения в памяти телефона.(базовый интент)
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        // Запускает выбор изображения памяти телефона с помощью постоянного кода.
+        activity.startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
 }
